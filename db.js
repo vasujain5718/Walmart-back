ff72d3ba644a71a3c262f4e4f40ea1622732f054
vasujain5718 <jain90662@gmail.com>
2025-07-03 17:52:53 +0530

    updated env

--- db.js
+++ db.js
@@ -1,7 +1,12 @@
-const mongouri="mongodb://localhost:27017/walmart";
+require('dotenv').config();
 const mongoose = require('mongoose');
-const contomongo=()=>{mongoose.connect(mongouri)
-  .then(() => console.log('MongoDB connected successfully'))
+
+const mongouri = process.env.MONGO_URI;
+
+const contomongo = () => {
+  mongoose.connect(mongouri)
+    .then(() => console.log('MongoDB connected successfully'))
     .catch(err => console.error('MongoDB connection error:', err));
-}
+};
+
 module.exports = contomongo;
