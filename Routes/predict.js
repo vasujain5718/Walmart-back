d47aa4238a844145e4f5415ede8011c236763493
vasujain5718 <jain90662@gmail.com>
2025-07-07 18:09:17 +0530

    update

--- Routes/predict.js
+++ Routes/predict.js
@@ -8,8 +8,14 @@ const ML_API_BASE = "https://walmart-ml.onrender.com";
 router.get("/:productId", async (req, res) => {
   try {
     const { productId } = req.params;
-    const response = await axios.get(`${ML_API_BASE}/predict/${productId}`);
-    res.json(response.data);
+    const response=await fetch(`${ML_API_BASE}/predict/${productId}`, {
+      method: "GET", 
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    res.json(data);
   } catch (err) {
     res.status(500).json({ error: "Error fetching ML prediction" });
   }
